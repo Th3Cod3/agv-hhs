e4c8e3c203738d73fa05104aa21b98e9447efaa9
JoonT0M26 <95414300+JoonT0M26@users.noreply.github.com>
2022-05-24 10:58:34 +0200

    Jonathan_test

--- main.c
+++ main.c
@@ -25,7 +25,7 @@ int main(void)
 {
     initGlobal();
     basic_outputMode(enableA, HIGH);
-    basic_outputMode(enableB, HIGH);
+    //basic_outputMode(enableB, HIGH);
 
     while (1) {
         DEBUG_SIGNAL
